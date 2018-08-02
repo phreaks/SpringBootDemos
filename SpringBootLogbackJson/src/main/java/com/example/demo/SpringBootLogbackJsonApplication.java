e049e0fefb474b255520146b1ec050a5ccd322a5
teddy <detlef.chen@frischware.net>
2018-08-02 06:59:12 +0200

    init

---
+++ SpringBootLogbackJson/src/main/java/com/example/demo/SpringBootLogbackJsonApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogbackJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLogbackJsonApplication.class, args);
+		
+		// https://github.com/logstash/logstash-logback-encoder
+		Logger logger = LoggerFactory.getLogger("SpringBootWithLogbackJson");
+	
+		String corId= "123-456-789";
+		
+		logger.info("foo: {}", keyValue("correlation-id", corId));
+		
+		Exception e = new NullPointerException("nPe");
+		logger.info("Fehler: {}", "oops", e);
+	}
+}
