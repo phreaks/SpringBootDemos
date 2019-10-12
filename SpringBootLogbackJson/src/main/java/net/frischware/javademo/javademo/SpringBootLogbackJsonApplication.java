58cdaa9c4c2c9c4d333aeeac003b61a04bff1827
Detlef Chen <hallo@frischware.net>
2019-10-12 20:09:11 +0200

    showcase logback with deletion

--- SpringBootLogbackJson/src/main/java/net/frischware/javademo/javademo/SpringBootLogbackJsonApplication.java
+++ SpringBootLogbackJson/src/main/java/net/frischware/javademo/javademo/SpringBootLogbackJsonApplication.java
@@ -2,25 +2,31 @@ package net.frischware.javademo.javademo;
 
 import static net.logstash.logback.argument.StructuredArguments.keyValue;
 
+import io.vavr.collection.Stream;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Random;
+
+@Slf4j
 @SpringBootApplication
 public class SpringBootLogbackJsonApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(SpringBootLogbackJsonApplication.class, args);
-		
-		// https://github.com/logstash/logstash-logback-encoder
-		Logger logger = LoggerFactory.getLogger("SpringBootWithLogbackJson");
-	
-		String corId= "123-456-789";
-		
-		logger.info("foo: {}", keyValue("correlation-id", corId));
-		
-		Exception e = new NullPointerException("nPe");
-		logger.info("Fehler: {}", "oops", e);
+
 	}
+
+
+	private static int getRandomNumberInRange(int min, int max) {
+		Random r = new Random();
+		return r.ints(min, (max + 1)).findFirst().getAsInt();
+
+	}
+
+
 }
